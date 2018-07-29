021ebdcff24baa27a53cb3c1349192677647f4f3
MvC07 <mona.voncontzen@gmail.com>
2018-07-30 02:40:42 +1000

    new website
    new website

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './main.css';
+
+class Home extends Component {
+  render() {
+      return(
+            <div className="headline">
+                Welcome to my website!
+            </div>
+      )
+  }
+}
+
+export default Home;
